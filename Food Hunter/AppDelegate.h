08049911d680fcdaf93e1e7cc3109589e2079f28
Matz <matz@Matzs-Mac.local>
2013-09-09 17:44:17 -0700

    Initial Commit

---
+++ Food Hunter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Food Hunter
+//
+//  Created by Matz on 9/9/13.
+//  Copyright (c) 2013 Matz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Food Hunter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Food Hunter
+//
+//  Created by Matz on 9/9/13.
+//  Copyright (c) 2013 Matz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
